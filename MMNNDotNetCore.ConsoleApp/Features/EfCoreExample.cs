be6f2317e906fe83f1f85ddbbf5490a2802e681a
marmarnaingnyein <marmarnaingnyein811@gmail.com>
2024-05-08 00:06:06 +0630

    - filter efcore

--- MMNNDotNetCore.ConsoleApp/Features/EfCoreExample.cs
+++ MMNNDotNetCore.ConsoleApp/Features/EfCoreExample.cs
@@ -6,11 +6,10 @@ namespace MMNNDotNetCore.ConsoleApp.Features;
 public class EfCoreExample
 {
     private DataGenerateService _dataGenerateService = new DataGenerateService();
-
+    private readonly AppDbContext _db = new AppDbContext();
     public void SelectAll()
     {
-        AppDbContext db = new AppDbContext();
-        List<BlogModel> lstBlog = db.Blogs.ToList();
+        List<BlogModel> lstBlog = _db.Blogs.ToList();
         foreach (var item in lstBlog)
         {
             _dataGenerateService.WriteDataList(item);
@@ -19,12 +18,26 @@ public class EfCoreExample
     
     public void SelectBy()
     {
-        string query = string.Empty;
-        BlogModel para = _dataGenerateService.GetFiltersBlog(out query);
+        BlogModel filter = _dataGenerateService.GetFiltersBlog();
+
+        List<BlogModel> lst = _db.Blogs.ToList();
+
+        if (filter.BlogId > 0)
+        {
+            lst = lst.Where(w => w.BlogId == filter.BlogId).ToList();
+        }
+        else if (!string.IsNullOrEmpty(filter.BlogTitle))
+        {
+            lst = lst.Where(w => w.BlogTitle == filter.BlogTitle).ToList();
+        }
+        else if (!string.IsNullOrEmpty(filter.BlogAuthor))
+        {
+            lst = lst.Where(w => w.BlogAuthor == filter.BlogAuthor).ToList();
+        }
         
-        // foreach (var item in lst)
-        // {
-        //     _dataGenerateService.WriteDataList(item);
-        // }
+        foreach (var item in lst)
+        {
+            _dataGenerateService.WriteDataList(item);
+        }
     }
 }
--- MMNNDotNetCore.ConsoleApp/Service/DataGenerateService.cs
+++ MMNNDotNetCore.ConsoleApp/Service/DataGenerateService.cs
@@ -191,4 +191,63 @@ public class DataGenerateService
 
         return filter;
     }
+    
+    public BlogModel GetFiltersBlog()
+    {
+        Console.WriteLine("Please choose your filter by...");
+        Console.WriteLine("1. Select by Blog Id");
+        Console.WriteLine("2. Select by Title");
+        Console.WriteLine("3. Select by Author");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        
+        while (choice is <= 0 or > 3)
+        {
+            Console.WriteLine("Enter your choice number:");
+            choice = Convert.ToInt32(Console.ReadLine());
+        }
+
+        string id = string.Empty;
+        string title = string.Empty;
+        string author = String.Empty;
+        switch (choice)
+        {
+            case 1:
+                while (string.IsNullOrEmpty(id))
+                {
+                    Console.WriteLine("Please enter Blog Id");
+                    id = Console.ReadLine()!;
+                }
+                break;
+            case 2:
+                while (string.IsNullOrEmpty(title))
+                {
+                    Console.WriteLine("Please enter title");
+                    title = Console.ReadLine()!;
+                }
+                break;
+            case 3:
+                while (string.IsNullOrEmpty(author))
+                {
+                    Console.WriteLine("Please enter author");
+                    author = Console.ReadLine()!;
+                }
+                break;
+        }
+
+        BlogModel filter = new BlogModel();
+        if (!string.IsNullOrEmpty(id))
+        {
+            filter.BlogId = Convert.ToInt32(id);
+        }
+        else if (!string.IsNullOrEmpty(title))
+        {
+            filter.BlogTitle = title;
+        }
+        else if (!string.IsNullOrEmpty(author))
+        {
+            filter.BlogAuthor = author;
+        }
+
+        return filter;
+    }
 }
