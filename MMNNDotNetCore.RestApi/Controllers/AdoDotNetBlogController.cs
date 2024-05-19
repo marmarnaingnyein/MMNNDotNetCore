f45fb48923c43fedaf3049523e5ad73f10294457
MarMarNaingNyein <marmarnaingnyein@acedatasystems.com>
2024-05-19 22:57:41 +0630

    -

--- MMNNDotNetCore.RestApi/Controllers/AdoDotNetBlogController.cs
+++ MMNNDotNetCore.RestApi/Controllers/AdoDotNetBlogController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MMNNDotNetCore.RestApi.Service;
 
 namespace MMNNDotNetCore.RestApi.Controllers;
 
 public class AdoDotNetBlogController : Controller
 {
-    
+    private readonly ValidationService _validation;
+
 }
