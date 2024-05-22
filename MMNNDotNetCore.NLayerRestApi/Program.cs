300fa61d008bc2343cae0709690467e9a87f93d6
MarMarNaingNyein <marmarnaingnyein811@gmail.com>
2024-05-22 22:43:21 +0630

    - add console httpclient

--- MMNNDotNetCore.NLayerRestApi/Program.cs
+++ MMNNDotNetCore.NLayerRestApi/Program.cs
@@ -1,11 +1,16 @@
+using MMNNDotNetCore.NLayerRestApi.Features.Blog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<BL_Blog>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
