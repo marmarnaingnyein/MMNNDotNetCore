7b46f04836feb96e920b523d91dbc793208a6734
MarMarNaingNyein <marmarnaingnyein811@gmail.com>
2024-05-23 23:57:39 +0630

    -

--- MMNNDotNetCore.ConsoleAppHttpClient/Program.cs
+++ MMNNDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -1,7 +1,14 @@
-﻿HttpClient client = new HttpClient();
-var response = await client.GetAsync("https://localhost:7253");
+﻿using MMNNDotNetCore.Models;
+using Newtonsoft.Json;
+
+HttpClient client = new HttpClient();
+var response = await client.GetAsync("https://localhost:7253/api/Blog");
 if (response.IsSuccessStatusCode)
 {
     string jsonStr = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(jsonStr);
+    List<BlogModel> lst = JsonConvert.DeserializeObject<List<BlogModel>>(jsonStr)!;
+    foreach (var item in lst)
+    {
+        Console.WriteLine(JsonConvert.SerializeObject(item));
+    }
 }
