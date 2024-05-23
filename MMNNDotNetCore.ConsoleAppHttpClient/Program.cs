7d588ed784cac3b17c60d0b06d0aeaa14a9e07bb
MarMarNaingNyein <marmarnaingnyein811@gmail.com>
2024-05-23 23:53:35 +0630

    -

--- MMNNDotNetCore.ConsoleAppHttpClient/Program.cs
+++ MMNNDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -1,36 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Newtonsoft.Json;
-
-Console.WriteLine("Hello, World!");
-
-string jsonData = await File.ReadAllTextAsync("data.json");
-MainDto? model = JsonConvert.DeserializeObject<MainDto>(jsonData);
-
-foreach (var item in model!.questions)
+﻿HttpClient client = new HttpClient();
+var response = await client.GetAsync("https://localhost:7253");
+if (response.IsSuccessStatusCode)
 {
-    Console.WriteLine(item.questionNo);
+    string jsonStr = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(jsonStr);
 }
-
-Console.ReadLine();
-
-public class MainDto
-{
-    public Questions[] questions { get; set; }
-    public Answers[] answers { get; set; }
-    public string[] numberList { get; set; }
-}
-
-public class Questions
-{
-    public int questionNo { get; set; }
-    public string questionName { get; set; }
-}
-
-public class Answers
-{
-    public int questionNo { get; set; }
-    public int answerNo { get; set; }
-    public string answerResult { get; set; }
-}
-
