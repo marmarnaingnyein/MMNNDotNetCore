48c0c4b42f73d4a11fe9d9b3b1ede57e821aad7c
MarMarNaingNyein <marmarnaingnyein811@gmail.com>
2024-05-24 00:10:40 +0630

    -

---
+++ MMNNDotNetCore.ConsoleAppHttpClient/ApiService.cs
@@ -0,0 +1,23 @@
+﻿using MMNNDotNetCore.Models;
+using Newtonsoft.Json;
+
+namespace MMNNDotNetCore.ConsoleAppHttpClient;
+
+public class ApiService
+{
+    private readonly HttpClient _client = new HttpClient() { BaseAddress = new Uri("https://localhost:7253") };
+    private readonly string _blogEndPoint = "api/blog";
+
+    public async Task<List<BlogModel>> GetBlogList()
+    {
+        List<BlogModel> lst = new List<BlogModel>();
+        var response = await _client.GetAsync(_blogEndPoint);
+        if (response.IsSuccessStatusCode)
+        {
+            string jsonStr = await response.Content.ReadAsStringAsync();
+            lst = JsonConvert.DeserializeObject<List<BlogModel>>(jsonStr)!;
+        }
+
+        return lst;
+    }
+}
--- MMNNDotNetCore.ConsoleAppHttpClient/HttpClientExamples.cs
+++ MMNNDotNetCore.ConsoleAppHttpClient/HttpClientExamples.cs
@@ -1,6 +1,20 @@
-﻿namespace MMNNDotNetCore.ConsoleAppHttpClient;
+﻿using MMNNDotNetCore.Models;
+
+namespace MMNNDotNetCore.ConsoleAppHttpClient;
 
 public class HttpClientExamples
 {
-    
+    private readonly ApiService _apiService = new ApiService();
+    public async Task GetList()
+    {
+        List<BlogModel> lst = await _apiService.GetBlogList();
+        foreach (var item in lst)
+        {
+            Console.WriteLine($"Blog Id : {item.BlogId}");
+            Console.WriteLine($"Blog Title : {item.BlogTitle}");
+            Console.WriteLine($"Blog Author : {item.BlogAuthor}");
+            Console.WriteLine($"Blog Content : {item.BlogContent}");
+            Console.WriteLine("===============================");
+        }
+    }
 }
