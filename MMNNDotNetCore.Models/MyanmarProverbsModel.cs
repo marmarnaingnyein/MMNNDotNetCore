adcf0515cba66c8297c6bd4742525d812dcde3fa
MarMarNaingNyein <marmarnaingnyein811@gmail.com>
2024-05-25 22:49:42 +0630

    - MyanmarProverbsController

---
+++ MMNNDotNetCore.Models/MyanmarProverbsModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MMNNDotNetCore.Models;
+
+public class MyanmarProverbsModel
+{
+    public MMProverbsTitle[] Tbl_MMProverbsTitle { get; set; }
+    public MMProverbsDetail[] Tbl_MMProverbs { get; set; }
+}
+
+public class MMProverbsTitle
+{
+    public int TitleId { get; set; }
+    public string TitleName { get; set; }
+}
+
+public class MMProverbsDetail
+{
+    public int TitleId { get; set; }
+    public int ProverbId { get; set; }
+    public string ProverbName { get; set; }
+    public string ProverbDesp { get; set; }
+}
---
+++ MMNNDotNetCore.NLayerRestApi/Features/MyanmarProverbs/MyanmarProverbsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MMNNDotNetCore.NLayerRestApi.Features.MyanmarProverbs;
+
+[Route("api/{controller}")]
+[ApiController]
+public class MyanmarProverbsController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> GetTitleList()
+    {
+        MyanmarProverbsModel model = await GetApiData();
+        return Ok(model.Tbl_MMProverbsTitle);
+    }
+
+    [HttpGet("{titleName}")]
+    public async Task<IActionResult> GetByTitleName(string titleName)
+    {
+        MyanmarProverbsModel model = await GetApiData();
+        MMProverbsTitle? title = model.Tbl_MMProverbsTitle.FirstOrDefault(w => w.TitleName == titleName);
+        if (title is null)
+        {
+            return Ok("Title is not found!");
+        }
+        
+        MMProverbsDetail? detail = model.Tbl_MMProverbs.FirstOrDefault(w => w.TitleId == title.TitleId);
+        if (detail is null)
+        {
+            return Ok("Detail is not found!");
+        }
+        return Ok(detail);
+    }
+
+    public async Task<IActionResult> Get(int titleId, int proverbId)
+    {
+        MyanmarProverbsModel model = await GetApiData();
+        MMProverbsDetail? detail = model.Tbl_MMProverbs
+            .FirstOrDefault(x => x.TitleId == titleId
+                                 && x.ProverbId == proverbId);
+        if (detail is null)
+        {
+            return Ok("Data is nt found!");
+        }
+
+        return Ok(detail);
+    }
+
+    private async Task<MyanmarProverbsModel> GetApiData()
+    {
+        MyanmarProverbsModel model = new MyanmarProverbsModel();
+        HttpClient client = new HttpClient();
+        var response = await client.GetAsync("https://raw.githubusercontent.com/sannlynnhtun-coding/Myanmar-Proverbs/main/MyanmarProverbs.json");
+        if (response.IsSuccessStatusCode)
+        {
+            string jsonStr = await response.Content.ReadAsStringAsync();
+            model = JsonConvert.DeserializeObject<MyanmarProverbsModel>(jsonStr)!;
+        }
+
+        return model;
+    }
+}
