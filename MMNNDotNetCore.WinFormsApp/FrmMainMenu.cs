9eb19bab602b4ad772e0383b7e9745d18db59587
MarMarNaingNyein <marmarnaingnyein811@gmail.com>
2024-05-27 23:56:23 +0630

    - list

---
+++ MMNNDotNetCore.WinFormsApp/FrmMainMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace MMNNDotNetCore.WinFormsApp;
+
+public partial class FrmMainMenu : Form
+{
+    public FrmMainMenu()
+    {
+        InitializeComponent();
+    }
+}
